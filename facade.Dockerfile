65c122da427d7cef5f29babe9669448c26945df6
DmytroMelnykm <dmytro.melnyk.it@gmail.com>
2023-05-22 01:52:16 +0300

    done four lab

--- facade.Dockerfile
+++ facade.Dockerfile
@@ -9,12 +9,15 @@ RUN apk update && apk add gcc python3-dev musl-dev
 
 WORKDIR /lab_1/facade
 COPY /lab_1/facade /lab_1/facade/
+COPY requirments-fast-api.txt /lab_1/facade/
 
 RUN python -m venv venv
 ENV PATH venv/bin:$PATH
 
 RUN pip install --upgrade pip
 RUN pip install -r requirments-fast-api.txt
+
+COPY __init__.py /lab_1/facade/venv/lib/python3.11/site-packages/hazelcast/__init__.py
 
 EXPOSE 8015/TCP
 ENTRYPOINT ["./entrypoint.sh"]
--- lab_1/facade/ports_logger.py
+++ lab_1/facade/ports_logger.py
@@ -1,28 +1,60 @@
 from random import choice
 from os import environ
 from aiohttp import ClientSession, ClientError
+from dataclasses import dataclass
 
 
-class LoggerPorts:
-    proxys_servies = [
-        f"http://api-loggin-1:{environ.get('PORT_LOGGER_1')}",
-        f"http://api-loggin-2:{environ.get('PORT_LOGGER_2')}",
-        f"http://api-loggin-3:{environ.get('PORT_LOGGER_3')}"
-    ]
+@dataclass
+class ServicesPorts:
+    proxys_servies: list[str]
     
-    @classmethod
-    async def choose_service(cls) -> str:
+    async def choose_service(self) -> str:
         while True:
-            choosen_serv = choice(cls.proxys_servies)
+            self.__choosen_serv = choice(self.proxys_servies)
             try:
-                await cls.get_massage_from_service(url=choosen_serv, method="GET")
-                return choosen_serv
+                await self.get_massage_from_service(method="GET")
+                return 
             except ClientError:
-                print("Chosen service not work {}".format(choosen_serv)) 
+                print("Chosen service not work {}".format(self.__choosen_serv)) 
                 
-    @staticmethod
-    async def get_massage_from_service(url: str, method: str, json: dict = None) -> dict:
-        async with ClientSession(url) as session:
-            async with session.request(url="/", method=method, json=json) as response:
+    async def get_massage_from_service(self, method: str = "GET", url: str = "/", json: dict = None) -> dict:
+        async with ClientSession(self.service) as session:
+            async with session.request(url=url, method=method, json=json) as response:
                 response.raise_for_status()
                 return await response.json()
+    
+    @property
+    def service(self):
+        return self.__choosen_serv
+            
+
+class FabricService:
+    
+    @classmethod
+    def get_service_logger(cls) -> ServicesPorts:
+        return ServicesPorts(proxys_servies=[
+            f"http://api-loggin-1:{environ.get('PORT_LOGGER_1')}",
+            f"http://api-loggin-2:{environ.get('PORT_LOGGER_2')}",
+            f"http://api-loggin-3:{environ.get('PORT_LOGGER_3')}"
+        ])  
+    
+    @classmethod
+    def get_service_message(cls) -> ServicesPorts:
+        return ServicesPorts(proxys_servies=[
+            f"http://api-message-1:{environ.get('PORT_MESSAGE_1')}",
+            f"http://api-message-2:{environ.get('PORT_MESSAGE_2')}"
+        ])
+    
+    @staticmethod
+    async def choosen_services(services: list[ServicesPorts]):
+        for service in services:
+            await service.choose_service()
+        
+    @classmethod
+    async def get_each_serivce(cls):
+        services = (
+            cls.get_service_logger(), 
+            cls.get_service_message()
+            )
+        await cls.choosen_services(services)
+        return services
