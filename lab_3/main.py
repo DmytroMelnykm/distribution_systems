c1fdafcc3fb6f018e1f5a985b752dec516f068e5
DmytroMelnykm <dmytro.melnyk.it@gmail.com>
2023-05-22 02:27:32 +0300

    fix counter. Done

--- lab_3/main.py
+++ lab_3/main.py
@@ -1,5 +1,5 @@
-from aiohttp import ClientSession, ClientError
-import asyncio
+from aiohttp import ClientSession
+from asyncio import new_event_loop, sleep
 
 
 async def get_massage_from_service(url: str, method: str, json: dict = None) -> dict:
@@ -11,7 +11,6 @@ async def get_massage_from_service(url: str, method: str, json: dict = None) -> 
 
 async def send_message():
     for number in range(15):
-    
         await get_massage_from_service(
                 url="http://0.0.0.0:8015",
                 method="POST",
@@ -22,14 +21,20 @@ async def send_message():
 
 
 async def get_all_massage(): 
-    print(await get_massage_from_service(
-            url="http://0.0.0.0:8015",
-            method="GET"
-        ))
+    for count_req in range(4):
+        
+        print("get {}".format(count_req))
+        print((await get_massage_from_service(
+                url="http://0.0.0.0:8015",
+                method="GET"
+            )).get("Response"))
+        
+        await sleep(2)
+    
 
         
 if __name__ == "__main__":
-    loop = asyncio.new_event_loop()
+    loop = new_event_loop()
     loop.run_until_complete(send_message())
     loop.run_until_complete(get_all_massage())
     
